a3e14baf92a3426d7d71848cc7696896d96486b8
Eric Ricketts <eric_ricketts@me.com>
2024-07-05 15:59:02 -0400

    update scoping rules code

--- mycode/Chapter3ObjectsEverywhere/scoping-rules/src/test/java/org/example/AppTest.java
+++ mycode/Chapter3ObjectsEverywhere/scoping-rules/src/test/java/org/example/AppTest.java
@@ -3,8 +3,7 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
@@ -20,6 +19,18 @@ public class AppTest {
       assertEquals(x, 10);
       // cannot make an assertion for y being out of scope
       // compiler flags the variable y before the test is run
+    }
+    { int x = 10;
+      {
+        int y = 11;
+        assertEquals(y, 11);
+        assertEquals(x, 10);
+        // we cannot do => int x = 10; Java does not allow redeclaration in an inner scope
+        // with a variable of the same name in an outer scope
+        x = 12; // we can reassign within an inner
+        assertEquals(12, x);
+      }
+        assertEquals(12, x); // retains value in outer scope after reassignment
     }
   }
 }
